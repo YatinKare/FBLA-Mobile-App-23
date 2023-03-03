d3acd8291b76691f92a8c12705d79b3ea59967e3
YatinK <yatin.kare@gmail.com>
2023-03-03 00:26:48 -0500

    Installed Basic Native Navigation Stack
    * Installed Required Libraries
    * Added Native Stack Navigator and Bottom Tab Navigator to App.tsx
    Created Two tab Navigators for Parent and Teacher

--- LearningLoop/App.tsx
+++ LearningLoop/App.tsx
@@ -5,7 +5,43 @@ import PhotosScreen from './screens/Photos';
 import UpcomingEventsScreen from './screens/UpcomingEvents';
 import UpdatePhotosScreen from './screens/UpdatePhotos';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TeacherTabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="UpdatePhotos" component={UpdatePhotosScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function ParentTabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Calendar" component={CalendarScreen} />
+      <Tab.Screen name="Photos" component={PhotosScreen} />
+      <Tab.Screen name="UpcomingEvents" component={UpcomingEventsScreen} />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
+
+
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Parent">
+      <Stack.Screen name="Teacher" component={TeacherTabNavigator} />
+      <Stack.Screen name="Parent" component={ParentTabNavigator} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+
   return (
     <UpdatePhotosScreen />
   );
